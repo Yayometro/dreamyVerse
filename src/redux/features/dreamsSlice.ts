ea07c7bc73e329f855be1a436d9190b251b6d04d
Jair Vazquez <gsfrido10@outlook.com>
2024-11-25 11:55:11 -0600

    removing the getTask from dreamsSlider

--- src/redux/features/dreamsSlice.ts
+++ src/redux/features/dreamsSlice.ts
@@ -11,15 +11,12 @@ export const dreamSlice = createSlice({
     name: "dream",
     initialState: dreamsInititalState,
     reducers: {
-        getTasks: (state) => {
-            return state.data
-        },
         setDreams: (state, action) => {
             return action.payload
         }
     }
 })
 
-export const { getTasks, setDreams } = dreamSlice.actions
+export const { setDreams } = dreamSlice.actions
 
 export default dreamSlice.reducer
